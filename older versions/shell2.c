17de66aac51b3b5354c7e4bdce89e5a5a90c0e02
Pawan <pawanbajaj2012@gmail.com>
2022-06-18 01:40:01 +0530

    Changed Input Method To fgets

--- older versions/shell2.c
+++ older versions/shell2.c
@@ -14,8 +14,6 @@ int main()
     while (1)
     {
         printf("this is the prompt message-> ");
-        // scanf("\n");
-        // scanf("%[^\n]s", command);
         fgets(command, MAX_SIZE_OF_COMMAND,stdin);
         if (!strcmp(command,"exit"))
         {
