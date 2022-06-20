cc3dd7ebee46c80063ed7172127cf2ee16ae4785
Pawan <pawanbajaj2012@gmail.com>
2022-06-20 22:22:42 +0530

    Reverted to scanf and added inbuilt commands

---
+++ older versions/shell3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+#define MAX_SIZE_OF_COMMAND 512
+char command[MAX_SIZE_OF_COMMAND];
+
+char *inbuilt_commands[] = {
+    "cd",
+    "ls",
+    "pwd",
+};
+
+int main() 
+{
+    while (1)
+    {
+        
+        printf("this is the prompt message-> ");
+        scanf("\n");
+        scanf("%[^\n]s", command);
+        if (!strcmp(command,"exit"))
+        {
+            exit(1);
+        }
+        
+        printf("%s\n", command);
+    }
+    
+}
