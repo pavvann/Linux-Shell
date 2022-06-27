de9f6f837c54741d35d37a065d2cb115ae633814
Pawan <pawanbajaj2012@gmail.com>
2022-06-27 22:42:54 +0530

    added welcome screen and prompt message with env variables

--- shell.c
+++ shell.c
@@ -10,18 +10,32 @@ static char* currentDirectory;
 #define MAX_SIZE_OF_COMMAND 512
 char command[MAX_SIZE_OF_COMMAND];
 
+void welcome()
+{
+    printf("\n\n\t-----------------------------------------\n");
+    printf("\t|---------------------------------------|\n");
+    printf("\t|--------------Linux Shell--------------|\n");
+    printf("\t|---------------------------------------|\n");
+    printf("\t|--------------Pawan Bajaj--------------|\n");
+    printf("\t|---------------------------------------|\n");
+    printf("\t|---------------------------------------|\n");
+    printf("\t-----------------------------------------\n\n\n");
+
+}
+
 void shellPrompt(){
 	// We print the prompt in the form "<user>@<host> <cwd> >"
 	char hostn[1204] = "";
 	gethostname(hostn, sizeof(hostn));
     // changing the color for prompt message
     printf("\033[0;32m");
-	printf("%s@%s %s > ", getenv("LOGNAME"), hostn, getcwd(currentDirectory, 1024));
+	printf("%s@%s %s > ", getenv("USER"), hostn, getcwd(currentDirectory, 1024));
     printf("\033[0m");
 }
 
 int main(int argc, char const *argv[], char **environ)
 {
+    welcome();
     while (1)
     {
         // setting environment variable for the child
